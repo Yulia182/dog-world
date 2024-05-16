9ccfea6546a910d3e4350a837dd098f440af61d1
Yulia Hernandez <iuliiahernandez2109@gmail.com>
2024-05-16 13:04:20 -0500

    added shopping cart features

--- script.js
+++ script.js
@@ -3,8 +3,22 @@
 import { buildProducts } from "./buildProduct.js";
 import { products } from "./products.js";
 
+// selectors
+const cart = document.querySelector(".shopping-cart");
+// click handler function
+const clickHandler = (e) => {
+  console.dir(e.target);
+  if (e.target.classList.contains("fa-cart-shopping")) {
+    console.dir(cart);
+    cart.classList.remove("hidden");
+  }
+  if (e.target.classList.contains("close")) {
+    cart.classList.add("hidden");
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   buildProducts(products);
   //   updateUI();
-  //   document.body.addEventListener("click");
+  document.body.addEventListener("click", clickHandler);
 });
